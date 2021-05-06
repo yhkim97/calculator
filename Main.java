8c8c74e4ffdc65d84de26202c7ae1b67f5d65ee0
choi-m-09 <audrbchzl@gmail.com>
2021-05-06 12:32:25 +0900

    메인클래스

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+    int num;
+    int num2;1
+    String s;
+
+   System.out.print("첫 번째 숫자 입력:");
+   num = scan.nextInt();
+      
+   System.out.print("\n계산식 문자입력:");
+   s = scan.next();
+
+   System.out.print("\n두 번째 숫자 입력:");
+   num2 = scan.nextInt();
+
+}
