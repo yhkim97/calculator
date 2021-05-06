6f15d76103628bca0c834c51e9d509e01e7f7322
hui97hui <doyangna12@gmail.com>
2021-05-06 12:54:57 +0900

    000

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+  public int split(int number1, int number2) {
+    return number1 * number2;
+  }
+
+  public int multiple(int number1, int number2) {
+    return number1 / number2;
+
+  }
+
+}
